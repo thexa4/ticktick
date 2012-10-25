f7e41a73a068c9dc4b40140b75105822466d7579
Max Maton <thexa4@gmail.com>
2012-10-25 15:12:23 +0200

    Created initial Layer

---
+++ TickTick/TickTick/Drawing/Layer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace TickTick.Drawing
+{
+    /// <summary>
+    /// Represents a depth slice of the current scene
+    /// Draws and updates all child components
+    /// </summary>
+    class Layer : DrawableGameComponent
+    {
+        /// <summary>
+        /// The subcomponents
+        /// </summary>
+        public List<DrawableGameComponent> Children { get; protected set; }
+
+        /// <summary>
+        /// Draws all components in this layer
+        /// </summary>
+        /// <param name="gameTime">The current gameTime</param>
+        public override void Draw(GameTime gameTime)
+        {
+            foreach (var child in Children)
+                child.Draw(gameTime);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            foreach (var child in Children)
+                child.Update(gameTime);
+        }
+
+        public Layer(Game game)
+            : base(game)
+        {
+
+        }
+    }
+}
