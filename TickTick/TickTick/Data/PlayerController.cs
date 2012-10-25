dbb3ce31fb0871f68760ca2ba005d2eefce91971
Max Maton <thexa4@gmail.com>
2012-10-25 17:03:12 +0200

    Created Mock PlayerController

---
+++ TickTick/TickTick/Data/PlayerController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TickTick.Data
+{
+    /// <summary>
+    /// This controls the player
+    /// </summary>
+    class PlayerController
+    {
+        
+    }
+}
