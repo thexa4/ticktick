9c6bb386558f490e30d1036ee81bcb8b823a7535
Max Maton <thexa4@gmail.com>
2012-10-25 16:03:52 +0200

    Added scaffold actors

---
+++ TickTick/TickTick/Drawing/Actors/Actor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace TickTick.Drawing.Actors
+{
+    class Actor : DrawableGameComponent
+    {
+        public Actor(Game game)
+            : base(game)
+        {
+
+        }
+    }
+}
---
+++ TickTick/TickTick/Drawing/Actors/WalkingActor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace TickTick.Drawing.Actors
+{
+    class WalkingActor : Actor
+    {
+        public WalkingActor(Game game)
+            : base(game)
+        {
+
+        }
+    }
+}
