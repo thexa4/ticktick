e5320732972ea16c771d88fd9f1c971641c34bed
Derk-Jan Karrenbeld <derk-jan@karrenbeld.info>
2012-10-26 14:50:02 +0200

    Normalisation for line endings

---
+++ TickTick/TickTick/Screens/LevelScreen.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TickTick.Data;
+using Microsoft.Xna.Framework;
+using TickTick.Services;
+
+namespace TickTick.Screens
+{
+    public class LevelScreen : GameScreen
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="game"></param>
+        /// <param name="data"></param>
+        public LevelScreen(Game game, Level data)
+            : base(game)
+        {
+
+        }
+    }
+}
