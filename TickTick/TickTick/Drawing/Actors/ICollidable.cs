c2a701a3210b29c65faed2249b01c59b040f63f3
Derk-Jan Karrenbeld <derk-jan@karrenbeld.info>
2012-11-04 16:02:51 +0100

    Added comments

--- TickTick/TickTick/Drawing/Actors/ICollidable.cs
+++ TickTick/TickTick/Drawing/Actors/ICollidable.cs
@@ -8,14 +8,47 @@ namespace TickTick.Drawing.Actors
 {
     public interface ICollidable
     {
+        /// <summary>
+        /// Position of the collidable
+        /// </summary>
         Vector2 Position { get; set; }
+
+        /// <summary>
+        /// Velocity of the collidable
+        /// </summary>
         Vector2 Velocity { get; set; }
+        
+        /// <summary>
+        /// Size of the collidable
+        /// </summary>
         Vector2 Size { get; set; }
+
+        /// <summary>
+        /// Is the collidable solid
+        /// </summary>
         bool IsSolid { get; set; }
+
+        /// <summary>
+        /// Is the collidable a platform
+        /// </summary>
         bool IsPlatform { get; set; }
 
+        /// <summary>
+        /// Starts touching a collider
+        /// </summary>
+        /// <param name="collidee"></param>
         void StartTouch(ICollidable collidee);
+
+        /// <summary>
+        /// Processes touches
+        /// </summary>
+        /// <param name="collidees"></param>
         void ProcessTouches(List<ICollidable> collidees);
+
+        /// <summary>
+        /// Stops touching a collider
+        /// </summary>
+        /// <param name="collidee"></param>
         void EndTouch(ICollidable collidee);
     }
 }
--- TickTick/TickTick/Drawing/Actors/Tile.cs
+++ TickTick/TickTick/Drawing/Actors/Tile.cs
@@ -9,13 +9,37 @@ namespace TickTick.Drawing.Actors
 {
     class Tile : LevelSprite, ICollidable
     {
+        /// <summary>
+        /// Tile is solid
+        /// </summary>
         public bool IsSolid { get; set; }
+
+        /// <summary>
+        /// Tile is a platform
+        /// </summary>
         public bool IsPlatform { get; set; }
+
+        /// <summary>
+        /// Velocity of the tile
+        /// </summary>
         public Vector2 Velocity { get; set; }
 
+        /// <summary>
+        /// Is a hot tile
+        /// </summary>
         public bool IsHot { get; set; }
+
+        /// <summary>
+        /// Is an ice tile
+        /// </summary>
         public bool IsIce { get; set; }
 
+        /// <summary>
+        /// Creates a new tile
+        /// </summary>
+        /// <param name="game"></param>
+        /// <param name="layer"></param>
+        /// <param name="assetname"></param>
         public Tile(Game game, Layer layer, string assetname)
             : base(game, layer, assetname)
         {
@@ -23,16 +47,28 @@ namespace TickTick.Drawing.Actors
             ((CollisionManager)Game.Services.GetService(typeof(CollisionManager))).Add(this);
         }
 
+        /// <summary>
+        /// Starts touching event
+        /// </summary>
+        /// <param name="collider"></param>
         public virtual void StartTouch(ICollidable collider)
         {
 
         }
 
+        /// <summary>
+        /// Ends touching event
+        /// </summary>
+        /// <param name="collider"></param>
         public virtual void EndTouch(ICollidable collider)
         {
 
         }
 
+        /// <summary>
+        /// Processes collisions
+        /// </summary>
+        /// <param name="colliders"></param>
         public virtual void ProcessTouches(List<ICollidable> colliders)
         {
 
