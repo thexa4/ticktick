60bda99c5e21bc2d28dac88a0d6be20b3b7c1756
Derk-Jan Karrenbeld <derk-jan@karrenbeld.info>
2012-11-04 16:01:26 +0100

    Finish touch now triggers state.end

--- TickTick/TickTick/Drawing/Actors/Finish.cs
+++ TickTick/TickTick/Drawing/Actors/Finish.cs
@@ -12,19 +12,30 @@ namespace TickTick.Drawing.Actors
     {
         public LevelState State { get; protected set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="game"></param>
+        /// <param name="layer"></param>
+        /// <param name="assetname"></param>
+        /// <param name="state"></param>
         public Finish(Game game, Layer layer, string assetname, LevelState state)
             : base(game, layer, assetname)
         {
             State = state;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="collider"></param>
         public override void StartTouch(ICollidable collider)
         {
             if (collider.GetType() != typeof(Player))
                 return;
 
             if (State.WaterDropsLeft == 0)
-                ScreenManager.ExitAll();
+                State.End();
         }
     }
 }
