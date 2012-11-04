13d37259761affcb33911cba36832826b653e5d1
Derk-Jan Karrenbeld <derk-jan@karrenbeld.info>
2012-11-04 16:00:40 +0100

    Added events to level state

--- TickTick/TickTick/Data/LevelState.cs
+++ TickTick/TickTick/Data/LevelState.cs
@@ -22,7 +22,14 @@ namespace TickTick.Data
         /// </summary>
         public Double TimeSpeed { get; set; }
 
-        //
+        /// <summary>
+        /// 
+        /// </summary>
+        public event EventHandler OnCompleted = delegate { }, OnLost = delegate { };
+
+        /// <summary>
+        /// 
+        /// </summary>
         public LevelState()
         {
             TimeSpeed = 1;
@@ -35,6 +42,20 @@ namespace TickTick.Data
         public virtual void Update(GameTime gameTime)
         {
             TimeLeft -= TimeSpeed * gameTime.ElapsedGameTime.TotalSeconds;
+
+            if (TimeLeft < 0)
+                End();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void End()
+        {
+            if (TimeLeft < 0)
+                OnLost.Invoke(this, EventArgs.Empty);
+            else if (WaterDropsLeft == 0)
+                OnCompleted.Invoke(this, EventArgs.Empty);
         }
     }
 }
